d86cb646cf734cc265a57903653a4f79cf2aaf46
jay-brdy <jaysirit@gmail.com>
2026-01-17 20:22:03 -0600

    chore(PlayWright): configure headed mode and slowmo

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  use: { baseURL: 'http://localhost:5173' }
+  use: {
+    baseURL: 'http://localhost:5173',
+    launchOptions: {
+      headless: false, //Run in headed mode
+      slowMo: 400, //slow down by 400ms
+    },
+  },
 });
